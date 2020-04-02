9d41bd78e251e92b887cab1abb65fd4fb02495ff
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-04-01 21:19:14 -0300

    Create button to show/hide the number of games played

--- src/Components/ListUsers.js
+++ src/Components/ListUsers.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-function ListUsers(props) {
-  return (
-    <ul>
-      {props.users.map(u => (
-        <li key={u.username}>
-          <p>Username: {u.username}</p>
-          <p>Games Played:{u.gamesPlayed}</p>
-        </li>
-      ))}
-    </ul>
-  )
+class ListUsers extends Component {
+  state = {
+    showGamesPlayed: true,
+    buttonText: 'Hide the Number of Games Played'
+  }
+
+  updateShowGames = () => {
+    this.setState(oldState => ({
+      showGamesPlayed: !oldState.showGamesPlayed,
+      buttonText: this.state.showGamesPlayed
+        ? 'Show the Number of Games Played'
+        : 'Hide the Number of Games Played'
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.updateShowGames}>{this.state.buttonText}</button>
+        <ul style={{ listStyleType: 'none' }}>
+          {this.props.users.map(u => (
+            <li key={u.username}>
+              <p>
+                {u.username} played{' '}
+                {this.state.showGamesPlayed ? u.gamesPlayed : '*'} games
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
 ListUsers.propTypes = {
